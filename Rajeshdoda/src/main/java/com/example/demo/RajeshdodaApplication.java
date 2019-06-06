abfd3d8de040c3b7c70577350439fccc2303c942
Rajji02 <rajat.agrawal@letsgettin.com>
2019-06-06 18:31:30 +0530

    before remote linkage

---
+++ Rajeshdoda/src/main/java/com/example/demo/RajeshdodaApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RajeshdodaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RajeshdodaApplication.class, args);
+		//syso
+	}
+
+}
